27bc8c285071c75590d0adefe513cc629ded4dac
Keamo-getswe <morobikmobi@gmail.com>
2023-12-14 17:05:08 +0200

    Started FreeCodeCamp Info Security Certification.
    * Set up project on local machine.
    * Complete Challenge 2.

--- myApp.js
+++ myApp.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const helmet = require('helmet')
 const app = express();
+
 
 
 
@@ -52,6 +54,9 @@ const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
+app.use(helmet())
+app.use(helmet.hidePoweredBy())
+
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
