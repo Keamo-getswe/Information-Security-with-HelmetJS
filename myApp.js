f850fdd0421de9b5ded9e569e4cba7af9d918194
Keamo-getswe <morobikmobi@gmail.com>
2023-12-15 15:21:38 +0200

    Fix bug in HSTS challenge

--- myApp.js
+++ myApp.js
@@ -11,7 +11,7 @@ app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
 
-local ninetyDaysInSeconds = 90*24*60*60;
+const ninetyDaysInSeconds = 90*24*60*60;
 app.use(
 	helmet.hsts({
 		maxAge: ninetyDaysInSeconds
